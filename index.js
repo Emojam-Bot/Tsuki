f43b1800a01c3f2023098b926a056f64e528c0ff
Sanjay Pavan <tsppavan7@gmail.com>
2021-11-06 14:24:53 +0530

    Easing up the bot

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const keepAlive = require("./server");
 const random = require('random');
 const fs = require('fs');
 const jsonfile = require('jsonfile');
+const Levels = require("discord-xp");
 
 require("dotenv").config();
 
@@ -19,18 +20,14 @@ if (fs.existsSync('stats.json')){
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
-  client.user.setActivity('withercubes.xyz')
+  client.user.setActivity('testing.xyz')
 })
 
-client.on('messageCreate', (message) => {
+client.on('messageCreate', async (message) => {
     // Ignores messages sent by or sent in from the list in ignore.json
-    if (message.author.bot){
-        return;
-    }
-
-    if (message.channel.name == ignoreList.channel1 || message.channel.name == ignoreList.channel2 || message.channel.name == ignoreList.channel3 || message.channel.name == ignoreList.channel4 || message.channel.name == ignoreList.channel5 || message.channel.name == ignoreList.channel6){
-        return;
-    }
+    if (!message.guild) return;
+    if (message.author.bot) return;
+    if (message.channel.name == ignoreList.channel1 || message.channel.name == ignoreList.channel2 || message.channel.name == ignoreList.channel3 || message.channel.name == ignoreList.channel4 || message.channel.name == ignoreList.channel5 || message.channel.name == ignoreList.channel6) return;
 
     if (message.guild.id in stats === false){
         stats[message.guild.id] = {};
@@ -39,59 +36,60 @@ client.on('messageCreate', (message) => {
     const guildStats = stats[message.guild.id];
     if (message.author.id in guildStats === false){
         guildStats[message.author.id] = {
-            xp: 0,
-            level: 0,
-            required_xp: 0,
             last_message: 0
         };
     }
 
     const userStats = guildStats[message.author.id];
     // Limit the XP for a user for every 1 minute
-    if (Date.now() - userStats.last_message > 60){
-    userStats.xp += random.int(5, 10); //give xp from 5 to 10
+    if (Date.now() - userStats.last_message > 60000){
+    const randomAmountOfXp = Math.floor(Math.random() * 29) + 1; // Min 1, Max 10
     userStats.last_message = Date.now();
-
-    const xpToNextLevel = 5 * Math.pow(userStats.level, 2) + 50 * userStats.level + 100; // MEE6 XP calculation
-    if (userStats.xp >= xpToNextLevel){
-        userStats.level++;
-        userStats.xp = userStats.xp - xpToNextLevel;
-        userStats.required_xp = xpToNextLevel
-        message.channel.send(message.author.toString() + ' has reached level ' + userStats.level); // Send message to user after increasing a level
+    const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomAmountOfXp);
+    if (hasLeveledUp) {   
+      const user = await Levels.fetch(message.author.id, message.guild.id);   
+      message.channel.send(`${message.author}, congratulations! You have leveled up to **${user.level}**. :tada:`);   
     }
-    userStats.required_xp = xpToNextLevel
     jsonfile.writeFileSync('stats.json', stats);
-
-    console.log(message.author.username + ' now has ' + userStats.xp);
-    console.log(xpToNextLevel + ' XP needed for next level');
 }
     const parts = message.content.split(' ');
 
     if (parts[0] === '!ping'){
-        message.reply("pong");
+        message.channel.send(`Bot Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
     }
 
-    if (parts[0] === '!ee'){
+    if (parts[0] === '!lb'){
+        const rawLeaderboard = await Levels.fetchLeaderboard(message.guild.id, 10); // We grab top 10 users with most xp in the current server.
+        if (rawLeaderboard.length < 1) return reply("Nobody's in leaderboard yet.");
+        const leaderboard = await Levels.computeLeaderboard(client, rawLeaderboard, true); // We process the leaderboard.
+        const lb = leaderboard.map(e => `${e.position}. ${e.username}#${e.discriminator}\nLevel: ${e.level}\nXP: ${e.xp.toLocaleString()}`); // We map the outputs.
+        const embed = new Discord.MessageEmbed()
+        .setColor("#58B9FF")
+        .setTitle("Server Leaderboard")
+        .setURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+        .addFields(
+            { name: "Leaderboard:", value: String(lb.join("\n\n")), inline: true}
+        )
+        message.channel.send({ embeds: [embed] });
+    }
+
+    if (parts[0] === '!rank'){
+        const target = message.mentions.users.first() || message.author; // Grab the target.
+        const user = await Levels.fetch(target.id, message.guild.id, true); // Selects the target from the database.
         const embed = new Discord.MessageEmbed()
         .setColor("#AEB2B5")
         .setTitle("Rank Card of " + message.author.username + '#' + message.author.discriminator )
         .setURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
         .setThumbnail(message.author.displayAvatarURL())
         .addFields(
-            { name: "Level", value: String(userStats.level), inline: true },
-            { name: "XP", value: String(userStats.xp) + "/" + String(userStats.required_xp), inline: true },
-            { name: "Markdown", value: "You can put all the *usual* **__Markdown__** inside of them.", inline: true }
+            { name: "Rank", value: String(user.position), inline: true},
+            { name: "Level", value: String(user.level), inline: true },
+            { name: "XP", value: String(user.xp) + "/" + String(Levels.xpFor(user.level + 1)), inline: true }
         )
-        /*
-        * Blank field, useful to create some space.
-        */
-        .addField("\u200b", "\u200b")
-        /*
-        * With Discord now allowing messages to contain up to 10 embeds, we need to put it in an array.
-        */
         message.channel.send({ embeds: [embed] });
     }
 })
 
 keepAlive()
+Levels.setURL(process.env.DB);
 client.login(process.env.BOTTOKEN)
